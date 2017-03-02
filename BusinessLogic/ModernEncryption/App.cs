99fafc6296e71a0fbd1e942049dc250d981ed0e0
Tobias Straub <hello@tobiasstraub.com>
2017-03-02 09:53:05 +0100

    Added BL project

---
+++ BusinessLogic/ModernEncryption/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernEncryption
+{
+    public class App
+    {
+        public void Start()
+        {
+         System.Diagnostics.Debug.WriteLine("Hello World!");   
+        }
+    }
+}
