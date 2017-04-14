82cc478c69598bc2dbcae039076248c7aa285b07
Lukas Ruf <lukas_ruf@gmx.net>
2017-04-14 13:30:43 +0200

    Added new database

--- Documents/krypto.sql
+++ Documents/krypto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 31. Mrz 2017 um 16:48
+-- Erstellungszeit: 14. Apr 2017 um 13:28
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 5.6.30
 
@@ -29,7 +29,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `message` (
   `id` int(11) NOT NULL,
   `chiffre` text NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
+  `keyNumber` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -41,7 +44,8 @@ CREATE TABLE `message` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `firstname` varchar(30) NOT NULL,
-  `surname` varchar(30) NOT NULL
+  `surname` varchar(30) NOT NULL,
+  `e-Mail` varchar(254) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -52,7 +56,9 @@ CREATE TABLE `user` (
 -- Indizes für die Tabelle `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`);
 
 --
 -- Indizes für die Tabelle `user`
@@ -74,6 +80,17 @@ ALTER TABLE `message`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
