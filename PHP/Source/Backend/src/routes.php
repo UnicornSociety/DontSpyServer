2ef18cd2cbcbb23e044e673c30ae946ba2a6c8a1
Mai Saito <mailegosaito@gmail.com>
2017-05-06 01:13:56 +0200

    edited files of classes and src by renaming, adding routes

--- PHP/Source/Backend/src/routes.php
+++ PHP/Source/Backend/src/routes.php
@@ -13,10 +13,51 @@ $app->post('/message/new', function($request, $response){
   "sender": "",
   "receiver":"",
   "message":"",
-  "key":"",
+  "keyNumber":""
 
 });
 
+$app->post('/message/new', function ($request, $response) {
+    $data = $request->getParsedBody();
+    $message_data = [];
+    $message_data['timestamp'] = $data['timestamp'];
+    $message_data['sender'] = filter_var($data['sender'], FILTER_SANITIZE_STRING);
+    $message_data['receiver'] = filter_var($data['receiver'], FILTER_SANITIZE_STRING);
+    $message_data['message'] = filter_var($data['message'], FILTER_SANITIZE_STRING);
+    $message_data['keyNumber'] = filter_var($data['keyNumber'], FILTER_SANITIZE_STRING);
+    $message_data['UserId'] = filter_var($data['userId'], FILTER_SANITIZE_STRING);
+    $message = new UserEntity($message_data);
+    $mapper = new UserMapper($this->db);
+    $mapper->save($message);
+    //$response = $response->withRedirect("/senvis/api/patient/" . $vitalSign_data['patientId'] . "/vitalsign");
+    return $response;
+});
+
+$app->get('/user', function ($request, $response) {
+
+  });
+
+$app->get('/message/{id}', function ($request, $response) {
+
+    });
+
+
+
+//Original
+
+$app->get('/user/{id}', function ($request, $response, $args) {
+    $patient_id = (int)$args['id'];
+    $mapper = new PatientMapper($this->db);
+    $patient = $mapper->getPatientById($patient_id);
+    return $response->withJson($patient);
+});
+
+$app->get('/message/{id}', function ($request, $response, $args) {
+    $patient_id = (int)$args['id'];
+    $mapper = new PatientMapper($this->db);
+    $patient = $mapper->getPatientById($patient_id);
+    return $response->withJson($patient);
+});
 
 $app->get('/patient', function ($request, $response) {
     $this->logger->info("Patients list");
