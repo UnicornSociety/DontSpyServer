c890968ef6c751cb16681e2723ff3971eb2dc42b
Mai Saito <mailegosaito@gmail.com>
2017-05-28 18:00:51 +0200

    edited permutation

--- BusinessLogic/ModernEncryption/Symbol.cs
+++ BusinessLogic/ModernEncryption/Symbol.cs
@@ -52,21 +52,21 @@ namespace ModernEncryption
             return "" + chiffreSplit1 + chiffreSplit2;
         }
 
-        /*//public int KeyDataReadIn()
-        // {
-            //int keyInput = Console.ReadLine();
-            //int keyInput;
-            //keyInput = 3;
-            //return keyInput;
-        //}
+        /*public int KeyDataReadIn()
+         {
+            int keyInput = Console.ReadLine();
+            int keyInput;
+           /keyInput = 3;
+            return keyInput;
+        }*/
 
-        public int ValueOfKeyDataReadIn()
+        /*public int ValueOfKeyDataReadIn()
         {
-            //int valueInput = Console.ReadLine();
+            int valueInput = Console.ReadLine();
             int valueInput;
             valueInput = 3;
             return valueInput;
-        }
+        }*/
 
         public static Dictionary<int, int> KeyTable  = new Dictionary<int, int>();
         public void Value(int valueInput)
@@ -172,25 +172,237 @@ namespace ModernEncryption
             KeyTable.Add(99, 69);
             KeyTable.Add(100, 70);
 
-            //foreach (KeyValuePair<int, int> key in KeyTable)
-            //{
-                //int valuePair = valueInput;
-            //}
-            //TODO: Variablen key und value definieren
-        }*/
+            KeyTable.Add(101, 263);
+            KeyTable.Add(102, 264);
+            KeyTable.Add(103, 265);
+            KeyTable.Add(104, 273);
+            KeyTable.Add(105, 250);
+            KeyTable.Add(106, 251);
+            KeyTable.Add(107, 252);
+            KeyTable.Add(108, 253);
+            KeyTable.Add(109, 254);
+            KeyTable.Add(110, 255);
+            KeyTable.Add(111, 256);
+            KeyTable.Add(112, 274);
+            KeyTable.Add(113, 275);
+            KeyTable.Add(114, 299);
+            KeyTable.Add(115, 300);
+            KeyTable.Add(116 ,    301);
+            KeyTable.Add(117, 302);
+            KeyTable.Add(118, 303);
+            KeyTable.Add(119, 304);
+            KeyTable.Add(120, 305);
+            KeyTable.Add(121, 306);
+            KeyTable.Add(122, 307);
+            KeyTable.Add(123, 308);
+            KeyTable.Add(124, 309);
+            KeyTable.Add(125, 310);
+            KeyTable.Add(126, 112);
+            KeyTable.Add(127, 113);
+            KeyTable.Add(128, 114);
+            KeyTable.Add(129, 115);
+            KeyTable.Add(130, 116);
+            KeyTable.Add(131, 117);
+            KeyTable.Add(132  ,   118);
+            KeyTable.Add(133, 119);
+            KeyTable.Add(134, 120);
+            KeyTable.Add(135, 121);
+            KeyTable.Add(136, 122);
+            KeyTable.Add(137, 107);
+            KeyTable.Add(138, 292);
+            KeyTable.Add(139, 101);
+            KeyTable.Add(140, 102);
+            KeyTable.Add(141, 103);
+            KeyTable.Add(142, 104);
+            KeyTable.Add(143, 105);
+            KeyTable.Add(144, 106);
+            KeyTable.Add(145, 286);
+            KeyTable.Add(146, 108);
+            KeyTable.Add(147, 109);
+            KeyTable.Add(148  ,   110);
+            KeyTable.Add(149, 111);
+            KeyTable.Add(150, 289);
+            KeyTable.Add(151, 288);
+            KeyTable.Add(152, 188);
+            KeyTable.Add(153, 189);
+            KeyTable.Add(154, 190);
+            KeyTable.Add(155, 191);
+            KeyTable.Add(156, 192);
+            KeyTable.Add(157, 193);
+            KeyTable.Add(158, 194);
+            KeyTable.Add(159, 195);
+            KeyTable.Add(160, 287);
+            KeyTable.Add(161, 123);
+            KeyTable.Add(162, 124);
+            KeyTable.Add(163, 125);
+            KeyTable.Add(164  ,   126);
+            KeyTable.Add(165, 127);
+            KeyTable.Add(166, 128);
+            KeyTable.Add(167, 129);
+            KeyTable.Add(168, 130);
+            KeyTable.Add(169, 131);
+            KeyTable.Add(170, 132);
+            KeyTable.Add(171, 133);
+            KeyTable.Add(172, 134);
+            KeyTable.Add(173, 135);
+            KeyTable.Add(174, 136);
+            KeyTable.Add(175, 137);
+            KeyTable.Add(176, 138);
+            KeyTable.Add(177, 139);
+            KeyTable.Add(178, 140);
+            KeyTable.Add(179, 141);
+            KeyTable.Add(180,     142);
+            KeyTable.Add(181, 143);
+            KeyTable.Add(182, 144);
+            KeyTable.Add(183, 145);
+            KeyTable.Add(184, 146);
+            KeyTable.Add(185, 147);
+            KeyTable.Add(186, 148);
+            KeyTable.Add(187, 149);
+            KeyTable.Add(188, 150);
+            KeyTable.Add(189, 151);
+            KeyTable.Add(190, 152);
+            KeyTable.Add(191, 153);
+            KeyTable.Add(192, 154);
+            KeyTable.Add(193, 155);
+            KeyTable.Add(194, 156);
+            KeyTable.Add(195, 157);
+            KeyTable.Add(196 ,    158);
+            KeyTable.Add(197, 159);
+            KeyTable.Add(198, 160);
+            KeyTable.Add(199, 161);
+            KeyTable.Add(200, 162);
+            KeyTable.Add(201, 163);
+            KeyTable.Add(202, 164);
+            KeyTable.Add(203, 165);
+            KeyTable.Add(204, 166);
+            KeyTable.Add(205, 167);
+            KeyTable.Add(206, 168);
+            KeyTable.Add(207, 169);
+            KeyTable.Add(208, 170);
+            KeyTable.Add(209, 171);
+            KeyTable.Add(210, 172);
+            KeyTable.Add(211, 173);
+            KeyTable.Add(212  ,   174);
+            KeyTable.Add(213, 175);
+            KeyTable.Add(214, 176);
+            KeyTable.Add(215, 177);
+            KeyTable.Add(216, 178);
+            KeyTable.Add(217, 179);
+            KeyTable.Add(218, 180);
+            KeyTable.Add(219, 181);
+            KeyTable.Add(220, 182);
+            KeyTable.Add(221, 183);
+            KeyTable.Add(222, 184);
+            KeyTable.Add(223, 185);
+            KeyTable.Add(224, 186);
+            KeyTable.Add(225, 187);
+            KeyTable.Add(226, 213);
+            KeyTable.Add(227, 214);
+            KeyTable.Add(228   ,  215);
+            KeyTable.Add(229, 216);
+            KeyTable.Add(230, 217);
+            KeyTable.Add(231, 218);
+            KeyTable.Add(232, 219);
+            KeyTable.Add(233, 220);
+            KeyTable.Add(234, 196);
+            KeyTable.Add(235, 197);
+            KeyTable.Add(236, 198);
+            KeyTable.Add(237, 199);
+            KeyTable.Add(238, 200);
+            KeyTable.Add(239, 201);
+            KeyTable.Add(240, 202);
+            KeyTable.Add(241, 203);
+            KeyTable.Add(242, 204);
+            KeyTable.Add(243, 205);
+            KeyTable.Add(244    , 206);
+            KeyTable.Add(245, 207);
+            KeyTable.Add(246, 208);
+            KeyTable.Add(247, 209);
+            KeyTable.Add(248, 298);
+            KeyTable.Add(249, 290);
+            KeyTable.Add(250, 291);
+            KeyTable.Add(251, 221);
+            KeyTable.Add(252, 222);
+            KeyTable.Add(253, 223);
+            KeyTable.Add(254, 224);
+            KeyTable.Add(255, 225);
+            KeyTable.Add(256, 226);
+            KeyTable.Add(257, 227);
+            KeyTable.Add(258, 228);
+            KeyTable.Add(259, 229);
+            KeyTable.Add(260,     230);
+            KeyTable.Add(261, 231);
+            KeyTable.Add(262, 210);
+            KeyTable.Add(263, 211);
+            KeyTable.Add(264, 212);
+            KeyTable.Add(265, 293);
+            KeyTable.Add(266, 294);
+            KeyTable.Add(267, 295);
+            KeyTable.Add(268, 296);
+            KeyTable.Add(269, 297);
+            KeyTable.Add(270, 232);
+            KeyTable.Add(271, 233);
+            KeyTable.Add(272, 234);
+            KeyTable.Add(273, 235);
+            KeyTable.Add(274, 236);
+            KeyTable.Add(275, 237);
+            KeyTable.Add(276 ,    238);
+            KeyTable.Add(277, 239);
+            KeyTable.Add(278, 240);
+            KeyTable.Add(279, 241);
+            KeyTable.Add(280, 242);
+            KeyTable.Add(281, 243);
+            KeyTable.Add(282, 244);
+            KeyTable.Add(283, 245);
+            KeyTable.Add(284, 246);
+            KeyTable.Add(285, 247);
+            KeyTable.Add(286, 248);
+            KeyTable.Add(287, 249);
+            KeyTable.Add(288, 266);
+            KeyTable.Add(289, 267);
+            KeyTable.Add(290, 268);
+            KeyTable.Add(291, 269);
+            KeyTable.Add(292, 270);
+            KeyTable.Add(293, 271);
+            KeyTable.Add(294, 272);
+            KeyTable.Add(295, 257);
+            KeyTable.Add(296, 258);
+            KeyTable.Add(297, 259);
+            KeyTable.Add(298, 260);
+            KeyTable.Add(299, 261);
+            KeyTable.Add(300, 262);
+            KeyTable.Add(301, 276);
+            KeyTable.Add(302, 277);
+            KeyTable.Add(303, 278);
+            KeyTable.Add(304, 279);
+            KeyTable.Add(305, 280);
+            KeyTable.Add(306, 281);
+            KeyTable.Add(307, 282);
+            KeyTable.Add(308 ,    283);
+            KeyTable.Add(309, 284);
+            KeyTable.Add(310, 285);
 
-        /*//Encryption
-            public int Permutation(int randomNumber)
+
+            /*foreach (KeyValuePair<int, int> key in KeyTable)
+            {
+                int valuePair = valueInput;
+            }
+            TODO: Variablen key und value definieren*/
+        }
+
+        //Encryption
+        public int Permutation(int randomNumber)
             {
                 for (randomNumber = 0; randomNumber < 100; randomNumber++)
                 {
                     if (KeyTable.ContainsKey(randomNumber))
                     {
                         randomNumber = KeyTable[randomNumber];
-                        //return randomNumber;
+                        return randomNumber;
                     }
                 }
-                return 1; //Platzhalter
+                return 1;
             }
 
             //Decryption
@@ -201,11 +413,11 @@ namespace ModernEncryption
                         if (KeyTable.ContainsKey(value))
                         {
                             value = KeyTable[value];
-                            //return value; 
+                            return value;
                         }
                     }
-                    return 1; //Platzhalter
-            }*/
+                return 1; 
+            }
 
 
 
