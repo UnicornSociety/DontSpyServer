b0b117c09ed38d2dadc16d27b60893ab666920c5
Tobias Straub <hello@tobiasstraub.com>
2017-06-09 16:54:49 +0200

    Changed build action

--- ClientApp/ModernEncryption/App.xaml.cs
+++ ClientApp/ModernEncryption/App.xaml.cs
@@ -16,7 +16,7 @@ namespace ModernEncryption
 		public App ()
 		{
 			InitializeComponent ();
-		    MainPage = new View.AnchorPage();
+		    MainPage = new View.LoginPage();
 
             //cryptoTest();
         }
