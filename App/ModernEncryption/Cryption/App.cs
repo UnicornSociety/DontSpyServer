5338143113fe7ee73dc542b916d9baf098fed620
Tobias Straub <hello@tobiasstraub.com>
2017-06-09 15:21:46 +0200

    Declare merge-application

---
+++ App/ModernEncryption/Cryption/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ModernEncryption
+{
+    public class App
+    {
+        public void Start()
+        {
+            
+        }
+    }
+}
