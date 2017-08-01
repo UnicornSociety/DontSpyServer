2bf01e601d7d3376fe1c1653ddbc1f3b2fef8be0
Lukas Ruf <lukas_ruf@gmx.net>
2017-08-01 12:12:28 +0200

    Worked on SetView

--- ClientApp/ModernEncryption/Presentation/View/LoginPage.xaml.cs
+++ ClientApp/ModernEncryption/Presentation/View/LoginPage.xaml.cs
@@ -13,6 +13,7 @@ namespace ModernEncryption.Presentation.View
         {
             InitializeComponent();
             _viewModel = new LoginPageViewModel();
+            _viewModel.SetView(this);
             BindingContext = _viewModel;
         }
     }
--- ClientApp/ModernEncryption/Presentation/ViewModel/LoginPageViewModel.cs
+++ ClientApp/ModernEncryption/Presentation/ViewModel/LoginPageViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ModernEncryption.Presentation.View;
 
 namespace ModernEncryption.Presentation.ViewModel
 {
     class LoginPageViewModel
     {
         public string Title { get; set; } = "LoginPage";
+        private LoginPage _view;
+
+        public void SetView(LoginPage view)
+        {
+            _view = view;
+        }
     }
 }
