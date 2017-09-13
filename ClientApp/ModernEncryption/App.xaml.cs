d723e78d9aae6285caa9538779d023de2bcec3c6
Tobias Straub <hello@tobiasstraub.com>
2017-09-13 07:13:06 +0200

    Navigation support for tabbed pages

--- ClientApp/ModernEncryption/App.xaml.cs
+++ ClientApp/ModernEncryption/App.xaml.cs
@@ -30,7 +30,7 @@ namespace ModernEncryption
 
             // DEBUGGING START
             CrossSecureStorage.Current.DeleteKey("RegistrationProcess");
-            MainPage = new ChatPage();
+            MainPage = new AnchorPage();
             // DEBUGGING END
 
             /*if (!CrossSecureStorage.Current.HasKey("RegistrationProcess"))
--- ClientApp/ModernEncryption/Presentation/View/AnchorPage.xaml.cs
+++ ClientApp/ModernEncryption/Presentation/View/AnchorPage.xaml.cs
@@ -7,14 +7,9 @@ namespace ModernEncryption.Presentation.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AnchorPage : TabbedPage
     {
-        private AnchorPageViewModel _viewModel;
-
         public AnchorPage()
         {
             InitializeComponent();
-            _viewModel = new AnchorPageViewModel();
-            //_viewModel.SetView(this);
-            BindingContext = _viewModel;
         }
     }
 }
