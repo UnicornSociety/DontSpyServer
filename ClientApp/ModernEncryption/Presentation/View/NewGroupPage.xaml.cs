2f93b0952f89fdd6dc203578e146b6f7deba5a1f
Mai Saito <mailegosaito@gmail.com>
2017-10-04 00:11:58 +0200

    worked on Icon (+ new group page)

---
+++ ClientApp/ModernEncryption/Presentation/View/NewGroupPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ModernEncryption.Presentation.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ModernEncryption.Presentation.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewGroupPage : ContentPage
+    {
+
+        public ContactPageViewModel ContactPageViewModel { get; set; }
+
+        public NewGroupPage(ContactPageViewModel viewModel)
+        {
+            ContactPageViewModel = viewModel;
+
+            InitializeComponent();
+
+            viewModel.SetView(this);
+            BindingContext = viewModel;
+        }
+
+    }
+}
