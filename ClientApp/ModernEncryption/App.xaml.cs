513ee7cbe24ef0d8df3cdbacf68091f51a2ff3c5
Mai Saito <mailegosaito@gmail.com>
2017-10-12 01:47:25 +0200

    worked on LoginPage, ContactPage, ChatOverviewPage

--- ClientApp/ModernEncryption/App.xaml.cs
+++ ClientApp/ModernEncryption/App.xaml.cs
@@ -36,7 +36,7 @@ namespace ModernEncryption
 
             // DEBUGGING START
             CrossSecureStorage.Current.DeleteKey("RegistrationProcess");
-            MainPage = new AnchorPage();
+            MainPage = new ChatPage();
             // DEBUGGING END
 
             /*if (!CrossSecureStorage.Current.HasKey("RegistrationProcess"))
