7bfcfa550462eee394f50929cfdc700f1175e35b
Mai Saito <mailegosaito@gmail.com>
2017-10-13 01:36:02 +0200

    worked on Views

--- ClientApp/ModernEncryption/App.xaml.cs
+++ ClientApp/ModernEncryption/App.xaml.cs
@@ -38,7 +38,7 @@ namespace ModernEncryption
             // DEBUGGING START
             CrossSecureStorage.Current.DeleteKey("RegistrationProcess");
             var test = new List<User>();//um Chat Page als Main Page zu nehmen
-            MainPage = new AnchorPage();
+            MainPage = new RegistrationPage();
             // DEBUGGING END
 
             /*if (!CrossSecureStorage.Current.HasKey("RegistrationProcess"))
