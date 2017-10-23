c41ad417d8b2a9f7f75e109029d39b510c4b12dd
Tobias Straub <hello@tobiasstraub.com>
2017-10-23 19:10:06 +0200

    Updated MySQL Dump

--- Documents/mysql_dump.sql
+++ Documents/mysql_dump.sql
@@ -1,13 +1,15 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 19. Mai 2017 um 16:34
--- Server-Version: 10.1.21-MariaDB
+-- Host: localhost:3306
+-- Erstellungszeit: 23. Okt 2017 um 19:09
+-- Server-Version: 10.0.31-MariaDB-cll-lve
 -- PHP-Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `krypto`
+-- Datenbank: `DontSpy`
 --
 
 -- --------------------------------------------------------
@@ -28,20 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `message` (
   `id` varchar(40) NOT NULL,
-  `message` text NOT NULL,
+  `messageHeader` varchar(254) NOT NULL,
+  `receivingChannel` varchar(40) NOT NULL,
   `timestamp` int(11) NOT NULL,
-  `sender` int(11) NOT NULL,
-  `receiver` int(11) NOT NULL
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `message`
---
-
-INSERT INTO `message` (`id`, `message`, `timestamp`, `sender`, `receiver`) VALUES
-('407285976ece4267b9476620fd697f69', 'qzxwg8w5dfwg', 1500639171, 1, 2),
-('407285976fef4267b9476620fd697f69', 'q2yug8w9d9wu', 1500639171, 2, 1),
-('407285976sds4267b9476620fd697f69', 'q1ypg8w8d5ws', 1500639171, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -53,16 +46,8 @@ CREATE TABLE `user` (
   `id` varchar(40) NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `surname` varchar(30) NOT NULL,
-  `eMail` varchar(254) NOT NULL
+  `email` varchar(254) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `user`
---
-
-INSERT INTO `user` (`id`, `firstname`, `surname`, `eMail`) VALUES
-('343285976ece4267b9476620fd697f69', 'Max', 'Mustermann', 'max.mustermann@gmx.net'),
-('789285976ece4267b9476620fd697f69', 'Petra', 'Fischer', 'petra.fischer@gmail.com');
 
 --
 -- Indizes der exportierten Tabellen
@@ -72,30 +57,15 @@ INSERT INTO `user` (`id`, `firstname`, `surname`, `eMail`) VALUES
 -- Indizes für die Tabelle `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `sender` (`sender`),
-  ADD KEY `receiver` (`receiver`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `message`
---
-ALTER TABLE `message`
-  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
