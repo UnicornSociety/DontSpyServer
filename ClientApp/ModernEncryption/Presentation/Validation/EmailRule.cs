c11bd2a134f2e7142f67e041377cb0b570e6d7e0
Tobias Straub <hello@tobiasstraub.com>
2017-10-27 22:15:32 +0200

    Started with validation of user inputs

---
+++ ClientApp/ModernEncryption/Presentation/Validation/EmailRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using ModernEncryption.Interfaces;
+
+namespace ModernEncryption.Presentation.Validation
+{
+    internal class EmailRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(T value)
+        {
+            return value != null && new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").Match(value as string).Success;
+        }
+    }
+}
