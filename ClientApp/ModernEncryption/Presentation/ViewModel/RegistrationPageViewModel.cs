c75df9a96be96c082f9bc92f3895d50024c33c4b
Tobias Straub <hello@tobiasstraub.com>
2017-10-28 14:44:31 +0200

    Implemented validation message support

--- ClientApp/ModernEncryption/Presentation/ViewModel/RegistrationPageViewModel.cs
+++ ClientApp/ModernEncryption/Presentation/ViewModel/RegistrationPageViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using System.ComponentModel;
 using System.Windows.Input;
 using ModernEncryption.Model;
+using ModernEncryption.Presentation.Validation;
+using ModernEncryption.Presentation.Validation.Rules;
 using ModernEncryption.Presentation.View;
 using ModernEncryption.Translations;
 using Xamarin.Forms;
 
 namespace ModernEncryption.Presentation.ViewModel
 {
-    public class RegistrationPageViewModel : INotifyPropertyChanged
+    public class RegistrationPageViewModel : ValidationBase, INotifyPropertyChanged
     {
         private RegistrationPage _view;
         private string _title = AppResources.RegistrationHeading;
+        private ValidatableObject<string> _firstname = new ValidatableObject<string>();
+        private ValidatableObject<string> _surname = new ValidatableObject<string>();
+        private ValidatableObject<string> _email = new ValidatableObject<string>();
         public ICommand SendVoucherCommand { protected set; get; }
+        public ICommand ValidateFirstnameCommand { protected set; get; }
+        public ICommand ValidateSurnameCommand { protected set; get; }
+        public ICommand ValidateEmailCommand { protected set; get; }
 
         public string Title
         {
@@ -24,18 +32,93 @@ namespace ModernEncryption.Presentation.ViewModel
             }
         }
 
+        public ValidatableObject<string> Firstname
+        {
+            get => _firstname;
+            set
+            {
+                if (_firstname == value) return;
+                _firstname = value;
+                OnPropertyChanged("Firstname");
+            }
+        }
+
+        public ValidatableObject<string> Surname
+        {
+            get => _surname;
+            set
+            {
+                if (_surname == value) return;
+                _surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+
+        public ValidatableObject<string> Email
+        {
+            get => _email;
+            set
+            {
+                if (_email == value) return;
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
         public RegistrationPageViewModel()
         {
+            AddValidations();
+
             SendVoucherCommand = new Command<object>(param =>
             {
-                var firstname = _view.FindByName<Entry>("firstname").Text;
-                var surname = _view.FindByName<Entry>("surname").Text;
-                var eMail = _view.FindByName<Entry>("eMail").Text;
-                var result = DependencyManager.UserService.CreateOwnUser(new User(firstname, surname, eMail));
+                if (!Validate()) return;
 
-                if (result)
-                    Application.Current.MainPage = new VoucherValidationPage();
+                var result = DependencyManager.UserService.CreateOwnUser(new User(Firstname.Value, Surname.Value, Email.Value));
+                if (result) Application.Current.MainPage = new VoucherValidationPage();
             });
+
+            ValidateFirstnameCommand = new Command<object>(param =>
+            {
+                ValidateFirstname();
+            });
+
+            ValidateSurnameCommand = new Command<object>(param =>
+            {
+                ValidateSurname();
+            });
+
+            ValidateEmailCommand = new Command<object>(param =>
+            {
+                ValidateEmail();
+            });
+        }
+
+        protected sealed override void AddValidations()
+        {
+            _firstname.Validations.Add(new StringLengthRule<string>(3, 30) { ValidationMessage = "error." });
+            _surname.Validations.Add(new StringLengthRule<string>(3, 30) { ValidationMessage = "error." });
+            _email.Validations.Add(new StringLengthRule<string>(6, 254) { ValidationMessage = "error." });
+            _email.Validations.Add(new EmailRule<string> { ValidationMessage = "error." });
+        }
+
+        protected override bool Validate()
+        {
+            return ValidateFirstname() && ValidateSurname() && ValidateEmail();
+        }
+
+        private bool ValidateFirstname()
+        {
+            return _firstname.Validate();
+        }
+
+        private bool ValidateSurname()
+        {
+            return _surname.Validate();
+        }
+
+        private bool ValidateEmail()
+        {
+            return _email.Validate();
         }
 
         public void SetView(RegistrationPage view)
